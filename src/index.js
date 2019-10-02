197636e37f7d7640a877265e1a6d2a1bae6330b2
vincent <alfa.king+git@gmail.com>
2019-10-02 10:54:44 +0800

    Adjust the test page.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Contacts from './contacts';
-import {deptTree, userList} from "./mockData";
+import { deptTree, userList } from './mockData';
 
 const updateSelectUsers = userList => {
   console.log(userList)
 };
 
-ReactDOM.render(<div className="App" style={{'padding': '10px', 'margin': '10px', height: '100%'}}>
+const users = [
+  { userId: 101,username:'aaa' }, { userId: 11,username: 'ccc' }
+];
+
+ReactDOM.render(<div className="App"
+                     style={{ 'padding': '10px', 'margin': '10px', height: '100%' }}>
   <header className="App-header">
     <h1 className="App-title">Welcome to React</h1>
   </header>
   <p className="App-intro">
     To get started, edit <code>src/App.js</code> and save to reload.
   </p>
-  <div style={{'padding': '10px', 'margin': '10px', height: '100%'}}>
-    <Contacts deptTree={deptTree} users={userList} loading={false} searchResult={[]}
+  <div style={{ 'padding': '10px', 'margin': '10px', height: '100%' }}>
+    <Contacts deptTree={deptTree} users={userList} loading={false}
+              defaultUserSelected={users}
               deptSearch={true} userSearch={true} deptCheckBox={false} searchUserPlaceholder={'HI'}
-              updateSelectUsers={updateSelectUsers} handleSearchUser={()=>userList} />
+              updateSelectUsers={updateSelectUsers} handleSearchUser={() => userList} />
   </div>
 </div>, document.getElementById('root'));
